6676aa0f4d3a11a69da7091b2c9cc69d40212ceb
Danielle Jasper <dnj547@live.com>
2019-03-15 21:30:38 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,10 +2,10 @@ require 'pry'
 class Hash
   def keys_of(**arguments)
     self.each do |key, value|
+      binding.pry
       if fetch(key) == arguments
         return fetch(key)
       end
-      binding.pry
     end
   end
 end
